0ec6ea562ce6d1cb1110133fbbd73fd3ea7f0ad9
kendall-hawk <58366663+kendall-hawk@users.noreply.github.com>
2025-06-10 14:03:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+Promise.all([
+  fetch('sentence.json').then(res => res.json()),
+  fetch('definitions.json').then(res => res.json())
+]).then(([sentenceData, definitions]) => {
+  const container = document.getElementById('sentence');
+  const tooltip = document.getElementById('tooltip');
 
+  sentenceData.wordIndexes.forEach(index => {
+    const wordInfo = definitions[index.toString()];
+    const structure = sentenceData.structure.find(item => item.index === index);
+    const span = document.createElement('span');
+    span.textContent = wordInfo.text + ' ';
+    span.classList.add('word');
+    if (structure) {
+      span.classList.add(structure.role); // e.g. subject, verb
+    }
+
+    span.onclick = () => {
+      tooltip.classList.remove('hidden');
+      tooltip.innerHTML = `
+        <strong>${wordInfo.text}</strong> (${wordInfo.pos})<br/>
+        <em>Definition:</em> ${wordInfo.definition}<br/>
+        <em>Image Sense:</em> ${wordInfo.imageSense}<br/>
+        <em>Example:</em> ${wordInfo.example}
+      `;
+    };
+
+    container.appendChild(span);
+  });
+});
